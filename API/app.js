37e7bfb6da9033cb2b4712b010ca0625197422a7
malbannai <moudhi.albannai@gamil.com>
2020-11-23 19:41:15 +0300

    port 8000 is showing the data

--- API/app.js
+++ API/app.js
@@ -1,5 +1,12 @@
-import express from "express";
-
+const tasks = require("./tasks");
+const cors = require("cors");
 const express = require("express");
 
 const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json(tasks);
+});
+
+app.listen(8000);
---
+++ API/tasks.js
@@ -0,0 +1,12 @@
+const tasks = [
+  {
+    id: 1,
+    task: "Gym",
+    status: true,
+    priority: "High",
+  },
+  { id: 2, task: "Attend the Bootcamp", status: true, priority: "Middle" },
+  { id: 3, task: "Finish the Mini project", status: false, priority: "Low" },
+];
+
+module.exports = tasks;
