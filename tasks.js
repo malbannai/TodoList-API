c90b85b2f4c814033f212ab24009f1213ab7b2d5
kalmansour <khaledalmansour24@gmail.com>
2020-11-23 19:01:53 +0300

    dummy data task

---
+++ tasks.js
@@ -0,0 +1,10 @@
+const tasks = [
+  {
+    id: 1,
+    name: "",
+    status: true,
+    priority: "High",
+  },
+  { id: 2, name: "", status: true, priority: "Middle" },
+  { id: 3, name: "", status: false, priority: "Low" },
+];
